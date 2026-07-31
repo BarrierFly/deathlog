72be531556c8c15ef62706f59491dd5391825da0
whatever <what@ever.com>
2026-07-31 21:35:54 +0800

    fix: restore death log UI text and list background

--- src/main/java/com/glisco/deathlog/client/gui/DeathListWidget.java
+++ src/main/java/com/glisco/deathlog/client/gui/DeathListWidget.java
@@ -3,6 +3,7 @@ package com.glisco.deathlog.client.gui;
 import com.glisco.deathlog.client.DeathInfo;
 import com.glisco.deathlog.storage.DirectDeathLogStorage;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import java.util.Objects;
 
@@ -16,6 +17,10 @@ public class DeathListWidget extends AlwaysSelectedEntryListWidget<DeathListEntr
         this.storage = s;
         this.restoreEnabled = c.player != null && c.player.hasPermissionLevel(4);
         this.refilter();
+    }
+
+    @Override
+    protected void drawMenuListBackground(DrawContext context) {
     }
 
     public boolean filter(String p) {
