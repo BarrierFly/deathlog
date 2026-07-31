05a047878674fadd1cce1f80a685121fb4eaf960
whatever <what@ever.com>
2026-07-31 11:54:27 +0800

    remove owo-lib dependency for MC 1.19.3

---
+++ src/main/java/com/glisco/deathlog/client/DeathLogConfigModel.java
@@ -0,0 +1,26 @@
+package com.glisco.deathlog.client;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DeathLogConfigModel {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Path PATH = FabricLoader.getInstance().getConfigDir().resolve("deathlog.json");
+    public boolean screenshotsEnabled = false;
+    public boolean useLegacyDeathDetection = false;
+    public static DeathLogConfigModel load() {
+        if (Files.exists(PATH)) {
+            try { return GSON.fromJson(Files.readString(PATH), DeathLogConfigModel.class); }
+            catch (IOException e) { return new DeathLogConfigModel(); }
+        }
+        return new DeathLogConfigModel();
+    }
+    public void save() {
+        try { Files.writeString(PATH, GSON.toJson(this)); }
+        catch (IOException ignored) {}
+    }
+}
