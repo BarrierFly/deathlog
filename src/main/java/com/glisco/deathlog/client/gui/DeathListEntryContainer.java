11745c505f96cc6858e87d028015742b99bb36bd
whatever <what@ever.com>
2026-08-01 16:49:57 +0800

    fix: restore death log GUI actions and runtime refmap

--- src/main/java/com/glisco/deathlog/client/gui/DeathListEntryContainer.java
+++ src/main/java/com/glisco/deathlog/client/gui/DeathListEntryContainer.java
@@ -13,7 +13,7 @@ import net.minecraft.text.Text;
 public class DeathListEntryContainer extends AlwaysSelectedEntryListWidget.Entry<DeathListEntryContainer> {
     private final TextRenderer textRenderer;
     private final DeathListWidget parent;
-    private final DeathInfo info;
+    private DeathInfo info;
 
     public DeathListEntryContainer(DeathListWidget parent, DeathInfo info) {
         this.info = info;
@@ -35,6 +35,8 @@ public class DeathListEntryContainer extends AlwaysSelectedEntryListWidget.Entry
     }
 
     public DeathInfo getInfo() { return info; }
+
+    public void updateInfo(DeathInfo info) { this.info = info; }
 
     @Override
     public Text getNarration() { return Text.of(info.getTitle().getString()); }
